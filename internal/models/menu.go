e3dcd69f9be9d2d0bba0895c8d7e30f8fd7018ef
AutMaple <autmaple609@qq.com>
2023-06-14 18:12:01 +0800

    实现查询菜单的接口

--- internal/models/menu.go
+++ internal/models/menu.go
@@ -36,81 +36,103 @@ func (m *Menu) PageQuery(tx *sql.Tx, offset, size int) ([]*Menu, error) {
   WHERE id >=(SELECT id FROM ums_menu WHERE enable = true ORDER BY id LIMIT ?,1)
   AND enable = true
   ORDER BY id LIMIT ?`
-  prepare, err := db.ToPrepare(tx, stmt)
-  if err != nil {
-    return nil, DetailError(err)
-  }
-  start := (offset - 1) * size
-  rows, err := prepare.Query(start, size)
-  if err != nil {
-    return nil, DetailError(err)
-  }
-  var menus []*Menu
-  for rows.Next() {
-    var menu Menu
-    err := rows.Scan(&menu.ID, &menu.Name, &menu.Url, &menu.Icon, &menu.Parent)
-    if err != nil {
-      return nil, DetailError(err)
-    }
-    menus = append(menus, &menu)
-  }
+	prepare, err := db.ToPrepare(tx, stmt)
+	if err != nil {
+		return nil, DetailError(err)
+	}
+	start := (offset - 1) * size
+	rows, err := prepare.Query(start, size)
+	if err != nil {
+		return nil, DetailError(err)
+	}
+	var menus []*Menu
+	for rows.Next() {
+		var menu Menu
+		err := rows.Scan(&menu.ID, &menu.Name, &menu.Url, &menu.Icon, &menu.Parent)
+		if err != nil {
+			return nil, DetailError(err)
+		}
+		menus = append(menus, &menu)
+	}
 	return menus, nil
 }
 
 func (m *Menu) Insert(tx *sql.Tx) error {
-  stmt := `insert into ums_menu(name,url,icon,parent) values(?,?,?,?)`
-  prepare,err := db.ToPrepare(tx, stmt)
-  if err != nil {
-    return DetailError(err)
-  }
-  result, err := prepare.Exec(&m.Name, &m.Url, &m.Icon, &m.Parent)
-  if err != nil {
-    return DetailError(err)
-  }
-  id, err := result.LastInsertId()
-  if err != nil {
-    return DetailError(err)
-  }
-  m.ID = int(id)
+	stmt := `insert into ums_menu(name,url,icon,parent) values(?,?,?,?)`
+	prepare, err := db.ToPrepare(tx, stmt)
+	if err != nil {
+		return DetailError(err)
+	}
+	result, err := prepare.Exec(&m.Name, &m.Url, &m.Icon, &m.Parent)
+	if err != nil {
+		return DetailError(err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return DetailError(err)
+	}
+	m.ID = int(id)
 	return nil
 }
 
 func (m *Menu) Update(tx *sql.Tx) error {
-  stmt := `update ums_menu name = ?, url = ?, icon = ?, parent = ? where id = ? and enable = true`
-  prepare, err := db.ToPrepare(tx, stmt)
-  if err != nil {
-    return DetailError(err)
-  }
-  result, err := prepare.Exec(m.Name, m.Url, m.Icon, m.Parent, m.ID)
-  if err != nil {
-    return DetailError(err)
-  }
-  affected, err := result.RowsAffected()
-  if err != nil {
-    return DetailError(err)
-  }
-  if affected == 0 {
-    return DetailError(sql.ErrNoRows)
-  }
+	stmt := `update ums_menu name = ?, url = ?, icon = ?, parent = ? where id = ? and enable = true`
+	prepare, err := db.ToPrepare(tx, stmt)
+	if err != nil {
+		return DetailError(err)
+	}
+	result, err := prepare.Exec(m.Name, m.Url, m.Icon, m.Parent, m.ID)
+	if err != nil {
+		return DetailError(err)
+	}
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return DetailError(err)
+	}
+	if affected == 0 {
+		return DetailError(sql.ErrNoRows)
+	}
 	return nil
 }
 
 func (m *Menu) Delete(tx *sql.Tx) error {
-  stmt := `update ums_menu set enable = false where id = ? `
-  prepare, err := db.ToPrepare(tx, stmt)
-  if err != nil {
-    return DetailError(err)
-  }
-  result, err := prepare.Exec(m.ID)
-  if err != nil {
-    return DetailError(err)
-  }
-  affected, err := result.RowsAffected()
-  if err != nil {
-    return DetailError(err)
-  }
-  if affected == 0 {
-    return DetailError(sql.ErrNoRows)
-  }
+	stmt := `update ums_menu set enable = false where id = ? `
+	prepare, err := db.ToPrepare(tx, stmt)
+	if err != nil {
+		return DetailError(err)
+	}
+	result, err := prepare.Exec(m.ID)
+	if err != nil {
+		return DetailError(err)
+	}
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return DetailError(err)
+	}
+	if affected == 0 {
+		return DetailError(sql.ErrNoRows)
+	}
 	return nil
+}
+
+func (m *Menu) QueryAll(tx *sql.Tx) ([]*Menu, error) {
+	stmt := `select id, name, url, icon, parent from ums_menu`
+	prepare, err := db.ToPrepare(tx, stmt)
+	if err != nil {
+		return nil, DetailError(err)
+	}
+	rows, err := prepare.Query()
+	if err != nil {
+		return nil, err
+	}
+	var menus []*Menu
+	for rows.Next() {
+		var m Menu
+		err := rows.Scan(&m.ID, &m.Name, &m.Url, &m.Icon, &m.Parent)
+		if err != nil {
+			return nil, DetailError(err)
+		}
+		menus = append(menus, &m)
+	}
+	return menus, nil
 }
